a107c2b4f8702fc9eec63f2070c8d79fdb0aa177
jafrin007 <57485749+jafrin007@users.noreply.github.com>
2020-10-02 00:08:54 +0530

    Create phone.cpp

---
+++ phone.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+struct phone
+{
+	int area_code,exchange,number;
+};
+int main()
+{
+	phone a,b;
+	a.area_code=212;
+	a.exchange=767;
+	a.number=8900;
+	cout<<"Enter your areacode,exchange, and number ";
+	cin>>b.area_code>>b.exchange>>b.number;
+	cout<<"My number is ("<<a.area_code<<") "<<a.exchange<<"-"<<a.number<<"\n";
+	cout<<"Your number is ("<<b.area_code<<") "<<b.exchange<<"-"<<b.number;
+	return 0;	
+}
