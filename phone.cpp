8735d802fa3e5ad57c84c28396e3060109595e40
devaprasadnm <56536197+devaprasadnm@users.noreply.github.com>
2020-10-02 00:12:46 +0530

    Update phone.cpp

--- phone.cpp
+++ phone.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
-struct phone
+struct Phone
 {
 	int area_code,exchange,number;
 };
 int main()
 {
-	phone a,b;
+	Phone a,b;
 	a.area_code=212;
 	a.exchange=767;
 	a.number=8900;
-	cout<<"Enter your areacode,exchange, and number ";
+	cout<<"Enter your Areacode,Exchange, and Number ";
 	cin>>b.area_code>>b.exchange>>b.number;
 	cout<<"My number is ("<<a.area_code<<") "<<a.exchange<<"-"<<a.number<<"\n";
 	cout<<"Your number is ("<<b.area_code<<") "<<b.exchange<<"-"<<b.number;
